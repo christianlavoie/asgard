0516df715808e2321e026ff5decea8626c444472
Chris Lavoie <clavoie@compilerworks.com>
2020-10-09 22:07:47 -0400

    Make lexer an iterator.

--- src/main.rs
+++ src/main.rs
@@ -1,58 +1,7 @@
-use std::fmt;
+use std::str::Chars;
 
 use rustyline::error::ReadlineError;
 use rustyline::Editor;
-
-#[derive(Debug)]
-enum BuiltIn {
-    Add,
-    Sub,
-    Mul,
-    Div,
-}
-
-impl fmt::Display for BuiltIn {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        match self {
-            BuiltIn::Add => { write!(f, "+") }
-            BuiltIn::Sub => { write!(f, "-") }
-            BuiltIn::Mul => { write!(f, "*") }
-            BuiltIn::Div => { write!(f, "/") }
-        }
-    }
-}
-
-#[derive(Debug)]
-enum Value {
-    Num(i64),
-    Str(String),
-    BuiltIn(BuiltIn),
-}
-
-impl fmt::Display for Value {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        match self {
-            Value::Num(i)      => { write!(f, "num({})", i) }
-            Value::Str(s)      => { write!(f, "str({})", s) }
-            Value::BuiltIn(bi) => { write!(f, "builtin({})", bi) }
-        }
-    }
-}
-
-#[derive(Debug)]
-enum Expr {
-    Fn,
-    Value(Value),
-}
-
-impl fmt::Display for Expr {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        match self {
-            Expr::Fn       => { write!(f, "Fn") }
-            Expr::Value(v) => { write!(f, "Value({})", v) }
-        }
-    }
-}
 
 #[derive(Clone,Copy,Debug)]
 enum ParserState {
@@ -60,37 +9,45 @@ enum ParserState {
     String
 }
 
-fn lexer(input: &str) -> Expr {
-    let mut stack: Vec<Expr> = Vec::new();
-    let mut ps = ParserState::Normal;
-    let it = &mut input.chars().peekable();
-    while let Some(&c) = it.peek() {
-        match (c, ps) {
-            (_,   ParserState::Normal) if c.is_ascii_whitespace() => { }
-            ('+', ParserState::Normal)   => { stack.push(Expr::Value(Value::BuiltIn(BuiltIn::Add))) }
-            ('-', ParserState::Normal)   => { stack.push(Expr::Value(Value::BuiltIn(BuiltIn::Sub))) }
-            ('*', ParserState::Normal)   => { stack.push(Expr::Value(Value::BuiltIn(BuiltIn::Mul))) }
-            ('/', ParserState::Normal)   => { stack.push(Expr::Value(Value::BuiltIn(BuiltIn::Div))) }
-            ('"', ParserState::Normal)   => {
-                ps = ParserState::String;
+struct Lexer<'e> {
+    input: &'e mut Chars<'e>,
+    parser_state: ParserState
+}
+
+#[derive(Clone,Debug)]
+enum LexItem {
+    OpenParen,
+    CloseParen,
+    Ident(String),
+    StringLit(String)
+}
+
+impl<'e> Iterator for Lexer<'e> {
+    type Item = LexItem;
+
+    fn next(&mut self) -> Option<LexItem> {
+        let it = &mut self.input;
+        match (it.next(), self.parser_state) {
+            (None,      _)                     => { None }
+            (Some(c),   ParserState::Normal) if c.is_ascii_whitespace() => { self.next() }
+            (Some('+'), ParserState::Normal)   => { Some(LexItem::Ident(String::from("+"))) }
+            (Some('-'), ParserState::Normal)   => { Some(LexItem::Ident(String::from("-"))) }
+            (Some('*'), ParserState::Normal)   => { Some(LexItem::Ident(String::from("*"))) }
+            (Some('/'), ParserState::Normal)   => { Some(LexItem::Ident(String::from("/"))) }
+            (Some('('), ParserState::Normal)   => { Some(LexItem::OpenParen) }
+            (Some(')'), ParserState::Normal)   => { Some(LexItem::CloseParen) }
+            (Some('"'), ParserState::Normal)   => {
+                self.parser_state = ParserState::String;
                 it.next();
                 let s = it.take_while(|c2| *c2 != '"').collect::<String>();
                 it.next();
-                stack.push(Expr::Value(Value::Str(s)));
+                Some(LexItem::StringLit(s))
             }
-            (_, _) => {
-                panic!("Unrecognized char {} in state {:?}", c, ps);
+            (c, ps) => {
+                panic!("Unrecognized char {:?} in state {:?}", c, ps);
             }
         }
     }
-
-    println!("stack: {:?}", stack);
-
-    Expr::Fn
-}
-
-fn eval(_input: &Expr) -> Value {
-    Value::Num(1)
 }
 
 fn main() {
@@ -113,10 +70,13 @@ fn main() {
         match readline {
             Ok(line) => {
                 rl.add_history_entry(line.as_str());
-                let r = read(&line);
-                let _e = eval(&r);
+                let r = Lexer {
+                    input: &mut line.chars(),
+                    parser_state: ParserState::Normal
+                };
 
                 println!("Line: {}", line);
+                println!("Tokens: {:?}", r.collect::<Vec<LexItem>>());
             },
             Err(ReadlineError::Interrupted) => {
                 println!("CTRL-C");
